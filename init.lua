ad5762862e9169f02a2ff73550aced8efa7c39f2
eyob721 <eyobmelkamu721@gmail.com>
2023-11-05 04:04:51 +0300

    feat: disable default options

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+-- Disable default options
+package.loaded["lazyvim.config.options"] = true
+
+-- Bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")

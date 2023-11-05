bfa7c7947d09a64d51a8ab5888ed48e2c19caf14
eyob721 <eyobmelkamu721@gmail.com>
2023-11-05 10:26:00 +0300

    feat: modify mini surround mapping

---
+++ lua/plugins/mini_surround.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        "echasnovski/mini.surround",
+        opts = {
+            mappings = {
+                add = "Sa",
+                delete = "Sd",
+                find = "Sf",
+                find_left = "SF",
+                highlight = "Sh",
+                replace = "Sr",
+                update_n_lines = "Sn",
+            },
+        },
+    },
+}

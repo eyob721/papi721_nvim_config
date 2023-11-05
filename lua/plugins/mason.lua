3c5d9f9639041716436d63fc4f6658bd5e2d3272
eyob721 <eyobmelkamu721@gmail.com>
2023-11-05 07:21:12 +0300

    feat: configure Lsp, linter and formatter for Bash
       - Use mason for Lsp, Linter and Formatter
       - nvim-lint is not required for bash

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -4,12 +4,11 @@ return {
         ensure_installed = {
             "bash-language-server", -- Bash Lsp
             "shellcheck", -- Bash Linter
+            "shfmt", -- Bash Formatter
             "clangd", -- C and C++ Lsp
             "pyright", -- Python Lsp
-            "stylua", -- Lua Formatter
-            "shfmt", -- Bash Formatter
-            "flake8", -- Python Linter, that has pycodestyel, pyflakes, ...
             "pydocstyle", -- Python Formatter for doctrings
+            "stylua", -- Lua Formatter
         },
     },
 }
---
+++ tests/simple_script.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+var="Hello"
+
+if true; then
+	echo $var
+fi

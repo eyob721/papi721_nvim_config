7feed25f67c7ef56a820a2406dd180b034fdb320
eyob721 <eyobmelkamu721@gmail.com>
2023-11-05 10:50:13 +0300

    chore: modify test file

--- tests/hello.c
+++ tests/hello.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * main - test program
+ *
+ * Return: 0 on sucess or 1 on error
+ */
 int main(void)
 {
+	int i;
+
+	(void)i;
+	printf("Hello World");
 	return (0);
 }

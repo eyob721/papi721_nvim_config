0c74b988c3331c7a92c37475aedc0e81962cd32b
eyob721 <eyobmelkamu721@gmail.com>
2023-11-06 17:58:49 +0300

    feat(lualine): adjust sections

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -53,29 +53,7 @@ return {
                     { Util.lualine.pretty_path() },
                 },
                 lualine_x = {
-                    -- stylua: ignore
-                    {
-                        function() return require("noice").api.status.command.get() end,
-                        cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-                        color = Util.ui.fg("Statement"),
-                    },
-                    -- stylua: ignore
-                    {
-                        function() return require("noice").api.status.mode.get() end,
-                        cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-                        color = Util.ui.fg("Constant"),
-                    },
-                    -- stylua: ignore
-                    {
-                        function() return "  " .. require("dap").status() end,
-                        cond = function () return package.loaded["dap"] and require("dap").status() ~= "" end,
-                        color = Util.ui.fg("Debug"),
-                    },
-                    {
-                        require("lazy.status").updates,
-                        cond = require("lazy.status").has_updates,
-                        color = Util.ui.fg("Special"),
-                    },
+                    { indentation },
                     {
                         "diff",
                         symbols = {
@@ -96,14 +74,11 @@ return {
                     },
                 },
                 lualine_y = {
-                    {
-                        "progress",
-                        separator = " ",
-                        padding = { left = 1, right = 0 },
-                    },
                     { "location", padding = { left = 0, right = 1 } },
                 },
-                lualine_z = { indentation },
+                lualine_z = {
+                    "progress",
+                },
             },
             extensions = { "neo-tree", "lazy" },
         }

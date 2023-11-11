abb6e9ffd9f1004166a0cbcdd73600d274635ad9
eyob721 <eyobmelkamu721@gmail.com>
2023-11-11 13:50:04 +0300

    feat: add key map for moving buffers

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- local map = vim.keymap.set
+local map = vim.keymap.set
 -- local delete = vim.keymap.del
 
 -- [[ Delete unwanted default keymaps ]]
+
+-- Move buffers on Buffer line
+map("n", "<C-H>", "<cmd>BufferLineMovePrev<cr>", { desc = "Move buffer left" })
+map("n", "<C-L>", "<cmd>BufferLineMoveNext<cr>", { desc = "Move buffer right" })

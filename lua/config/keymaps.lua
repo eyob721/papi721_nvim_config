8bc2aa4df7939a906811a5e114b280eb1201c9a3
eyob721 <eyobmelkamu721@gmail.com>
2023-11-16 06:55:21 +0300

    feat: add keybinding for moving buffers on the bufferline

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,10 +3,15 @@
 -- Add any additional keymaps here
 
 local map = vim.keymap.set
--- local delete = vim.keymap.del
+local delete = vim.keymap.del
 
 -- [[ Delete unwanted default keymaps ]]
+-- [LazyVim Default] Move to window using the <ctrl> hjkl keys
+-- delete("n", "C-H")
+-- delete("n", "C-J")
+-- delete("n", "C-K")
+-- delete("n", "C-L")
 
 -- Move buffers on Buffer line
-map("n", "<C-H>", "<cmd>BufferLineMovePrev<cr>", { desc = "Move buffer left" })
-map("n", "<C-L>", "<cmd>BufferLineMoveNext<cr>", { desc = "Move buffer right" })
+map("n", "<C-h>", "<cmd>BufferLineMovePrev<cr>", { desc = "Move buffer left" })
+map("n", "<C-l>", "<cmd>BufferLineMoveNext<cr>", { desc = "Move buffer right" })

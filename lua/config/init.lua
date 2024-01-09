a78969c6007936187ef594390ef8a48fa18ed44d
eyob721 <eyobmelkamu721@gmail.com>
2024-01-09 11:43:16 +0300

    feat: install the lazy plugin manager

---
+++ lua/config/init.lua
@@ -0,0 +1,25 @@
+-- [[ Install the Lazy Plugin Manager ]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to set the `mapleader` before loading any plugins
+vim.g.mapleader = " "
+vim.g.maplocalleaedr = "\\"
+
+-- Load the lazy plugin manager, and your plugins
+require("lazy").setup({
+  "folke/which-key.nvim",
+  { "folke/neoconf.nvim", cmd = "Neoconf" },
+  "folke/neodev.nvim",
+})
